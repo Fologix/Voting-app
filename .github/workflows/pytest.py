4d10d6e46dd4899b5d23f312404e92307ecad356
Fologix <yassine.verriez@hotmail.com>
2023-11-14 18:50:15 +0100

    f

--- .github/workflows/pytest.py
+++ .github/workflows/pytest.py
@@ -1,6 +1,6 @@
 # test_app.py
 import pytest
-from app import app  # Importez votre application Flask ici
+from app import app  
 
 @pytest.fixture
 def client():
@@ -12,7 +12,4 @@ def test_vote(client):
     # Simuler un vote pour l'option A
     response = client.post('/vote', data={'vote': 'A'})
     assert response.status_code == 200
-    assert b'Merci pour votre vote!' in response.data  # Vérifiez la réponse de votre application
-
-# Cette fonction pourrait vérifier l'état de la base de données ou tout autre
-# résultat attendu de l'action de vote, selon la logique de votre application.
+    assert b'Merci pour votre vote!' in response.data  
